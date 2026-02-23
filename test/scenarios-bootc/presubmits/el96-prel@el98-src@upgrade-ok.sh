0b1a8c84d04dbb12103695fe855d9573eeb26c16
Gregory Giguashvili <ggiguash@redhat.com>
2026-02-23 14:11:29 +0200

    Update presubmit scenarios

--- test/scenarios-bootc/presubmits/el96-prel@el98-src@upgrade-ok.sh
+++ test/scenarios-bootc/presubmits/el96-prel@el98-src@upgrade-ok.sh
@@ -21,7 +21,7 @@ scenario_run_tests() {
     exit_if_image_not_found "${start_image}"
 
     run_tests host1 \
-        --variable "TARGET_REF:rhel96-bootc-source" \
+        --variable "TARGET_REF:rhel98-bootc-source" \
         --variable "BOOTC_REGISTRY:${MIRROR_REGISTRY_URL}" \
         suites/upgrade/upgrade-successful.robot
 }
